06a6697aef1f1845fc110b625aa58a50140b276d
Yufei <taoyufeih@me.com>
2019-01-16 20:05:04 -0800

    Yufei test 1from xcode

---
+++ cs30project/cs30project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cs30project
+//
+//  Created by Yufei Tao on 1/16/19.
+//  Copyright © 2019 Yufei Tao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
