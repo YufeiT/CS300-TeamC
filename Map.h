040ec19b5b6e57fdbf2ac4f00dc0d6bf41669e5c
Shannon Keola <skeola@pdx.edu>
2019-01-22 15:58:21 -0800

    Updated Map.h and removed temp file

--- Map.h
+++ Map.h
@@ -2,9 +2,35 @@
 CS300 - Team C
 January 22, 2018*/
 
+struct Tile{
+	/*Char defines the type of Tile
+	M = Mountain
+	W = Water
+	T = Tree
+	etc*/
+	char type;
+	/*Maybe have an Obstruction object
+	Can check if there is an obstruction or not if null
+	Can use this object to define what tools work on it
+	Obstruction* obs;*/
+};
+
+/*
+struct Obstruction{
+	char* type;		//eg Boulder, Log, etc
+};
+*/
+
 class Map{
 public:
+	// Constructors
+	Map();
+	Map(int size,Player* p);
+
+	//Draws the map to the screen
+	void display();
 
 private:
-
-}
+	Tile** map;		//2d array of Tile objects
+	Player* p1;		//Pointer to the Player
+};
--- helloAsher.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main(){
-    cout << "Hello World from Asher\n";
-    return 1;
-}
