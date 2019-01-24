fd72b0ab7716110af75237b96252dc61c69bcfab
Roper Asher <roper@pdx.edu>
2019-01-23 20:17:19 -0800

    deleting 4 files

--- helloAsherTake2.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-void main(){
-    cout << "Hello World from Asher. Take 2\n";
-    cout << "Hello World from Asher. Take 3\n";
-}
--- take3.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-void main(){
-    cout << "Asher: 3rd try to submit to gitHub\n";
-}
