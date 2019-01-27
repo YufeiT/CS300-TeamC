b6094a3cce886c3189ef6aa2d76bd897eaa15560
Roper Asher <roper@pdx.edu>
2019-01-26 17:38:16 -0800

    added Movement.cpp and Movement.h 1/26 Asher Roper

---
+++ Movement_1-26-2019/Movement.cpp
@@ -0,0 +1,53 @@
+#include "Movement.h"
+
+Movement::Movement(){
+    mapColumnSize = 0;
+    mapRowSize = 0;
+}
+
+Movement::Movement(int column, int row){
+    mapColumnSize = column;
+    mapRowSize = row;
+}
+
+int Movement::north(int y){
+    if(checkYBounds(y++))
+        return y++;
+    else
+        return 0;
+}
+
+int Movement::south(int y){
+    if(checkYBounds(y--))
+        return y--;
+    else
+        return 0;
+}
+
+int Movement::east(int x){
+    if(checkXBounds(x++))
+        return x++;
+    else
+        return 0;
+}
+
+int Movement::west(int x){
+    if(checkXBounds(x--))
+        return x--;
+    else
+        return 0;
+}
+
+bool Movement::checkXBounds(int location){
+    if((location >= this->mapRowSize) || (location < 0))
+        return false;
+    else
+        return true;
+}
+
+bool Movement::checkYBounds(int location){
+    if((location >= this->mapColumnSize) || (location < 0))
+        return false;
+    else
+        return true;
+}
