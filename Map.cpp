003cc8e18cb0ee00e4edef13890dfe4665aebac1
Shannon Keola <skeola@pdx.edu>
2019-01-29 17:09:04 -0800

    Working movement w/ map display and vision updating

--- Map.cpp
+++ Map.cpp
@@ -3,29 +3,61 @@
 Map::Map(){
 	dimensions = 20;
 	map = generateMap();
-	p1 = new Player();
-	p1->x = dimensions/2;
-	p1->y = dimensions/2;
+	playerLoc[0] = 0;	//Player starts at 0,0
+	playerLoc[1] = 0;
 }
 
-Map::Map(int size,Player* p){
+Map::Map(int size){
 	dimensions = size;
 	map = generateMap();
-	p1 = p;
+	playerLoc[0] = 0;	//Player starts at 0,0
+	playerLoc[1] = 0;
 }
 
 Map::~Map(){
-	delete p1;
 	for(int i=0;i<dimensions;++i){
 		delete map[i];
 	}
 	delete map;
 }
 
+//MOVEMENT
+bool Map::move_north(){
+	if(playerLoc[0]>0){
+		--playerLoc[0];
+		return true;
+	}
+	else return false;
+}
+
+bool Map::move_west(){
+	if(playerLoc[1]>0){
+		--playerLoc[1];
+		return true;
+	}
+	else return false;
+}
+
+bool Map::move_east(){
+	if(playerLoc[1]<dimensions-2){
+		++playerLoc[1];
+		return true;
+	}
+	else return false;
+}
+
+bool Map::move_south(){
+	if(playerLoc[0]<dimensions-2){
+		++playerLoc[0];
+		return true;
+	}
+	else return false;
+}
+
 void Map::display(){
 	for(int i=0;i<dimensions;++i){
 		for(int j=0;j<dimensions;++j){
-			if(p1->x == i && p1->y == j) //If this Tile is the player
+			if(playerLoc[0]==i && playerLoc[1]==j) //If this Tile is the player
 				std::cout<<"O ";
 			else{
 				if(map[i][j].beenSeen)
@@ -34,13 +66,14 @@ void Map::display(){
 					std::cout<<"X ";
 			}
 		}
+		std::cout<<" "<<i;
 		std::cout<<'\n';
 	}
 }
 
 void Map::update(){
-	for(int i=p1->x-1;i<=p1->x+1;++i){
-		for(int j=p1->y-1;j<=p1->y+1;++j){
+	for(int i=playerLoc[0]-1;i<=playerLoc[0]+1;++i){
+		for(int j=playerLoc[1]-1;j<=playerLoc[1]+1;++j){
 			if(i>=0 && j>=0 && j<dimensions && i<dimensions)
 				map[i][j].beenSeen = true;
 		}
@@ -60,6 +93,7 @@ Tile** Map::generateMap(){
 		for(int k=0;k<dimensions;++k){
 			newMap[j][k].type = inFile.get();
 			newMap[j][k].beenSeen = false;
+			newMap[j][k].obstacle = NULL;
 		}
 		inFile.ignore(1);
 	}
--- Map.h
+++ Map.h
@@ -5,6 +5,11 @@ January 22, 2018*/
 #include <iostream>
 #include <fstream>
 
+struct Obstruction{
+	char* type;		//eg Boulder, Log, etc
+	char* tool;		//The tool to remove it
+};
+
 struct Tile{
 	/*Char defines the type of Tile
 	M = Mountain
@@ -13,39 +18,33 @@ struct Tile{
 	etc*/
 	char type;
 	bool beenSeen;
-	/*Maybe have an Obstruction object
-	Can check if there is an obstruction or not if null
-	Can use this object to define what tools work on it
-	Obstruction* obs;*/
+	Obstruction* obstacle;
 };
-
-struct Player{
-	int x;
-	int y;
-};
-
-/*
-struct Obstruction{
-	char* type;		//eg Boulder, Log, etc
-};
-*/
 
 class Map{
 public:
 	// Constructors
 	Map();
-	Map(int size,Player* p);
+	Map(int size);
 	~Map();
 
-	//Draws the map to the screen
+	//Player movement
+	bool move_north();
+	bool move_west();
+	bool move_east();
+	bool move_south();
+
+	//Draws the Map and Player to the screen
 	void display();
+
+	//Flips Tiles' beenSeen to true if adjacent to Player
 	void update();
 
 private:
 	Tile** map;		//2d array of Tile objects
-	Player* p1;		//Pointer to the Player
+	int playerLoc[2];	//Location of the Player
 	int dimensions; //Max dimensions of the map
 
-	//Generates map of size s
+	//Generates map of size sxs
 	Tile** generateMap();
 };
